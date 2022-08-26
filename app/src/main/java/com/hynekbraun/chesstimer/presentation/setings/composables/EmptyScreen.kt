119c7ec2c99879acac5b63f07dd9826465255c8d
resonus <braun.hynek@gmail.com>
2022-08-26 04:08:58 +0200

    Basic ui for settings

---
+++ app/src/main/java/com/hynekbraun/chesstimer/presentation/setings/composables/EmptyScreen.kt
@@ -0,0 +1,20 @@
+package com.hynekbraun.chesstimer.presentation.setings.composables
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.hynekbraun.chesstimer.R
+
+@Composable
+fun EmptyScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = stringResource(R.string.empty_time_settings))
+    }
+}
