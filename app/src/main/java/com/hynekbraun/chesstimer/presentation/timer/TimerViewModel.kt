500db04c2b2674ea7ce859ef93625601b79392a3
resonus <braun.hynek@gmail.com>
2022-09-03 21:39:51 +0200

    Working logic to get saved time from dataStore, but it loads only for the first time user opens the app = it does not update when changed.

--- app/src/main/java/com/hynekbraun/chesstimer/presentation/timer/TimerViewModel.kt
+++ app/src/main/java/com/hynekbraun/chesstimer/presentation/timer/TimerViewModel.kt
@@ -2,22 +2,33 @@ package com.hynekbraun.chesstimer
 
 import android.os.CountDownTimer
 import android.util.Log
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hynekbraun.chesstimer.data.local.timedatastore.TimeDataStore
+import com.hynekbraun.chesstimer.domain.TimeRepository
+import com.hynekbraun.chesstimer.presentation.timer.util.InitialTimer
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 private const val COUNTDOWN_INTERVAL = 1000L
-private const val START_TIME_LONG = 300000000L
 
 @HiltViewModel
-class TimerViewModel @Inject constructor() : ViewModel() {
+class TimerViewModel @Inject constructor(
+    private val repository: TimeRepository,
+    private val dataStore: TimeDataStore
+) : ViewModel() {
 
     var currentTurn = CurrentTurn.NOBODY
-        private set
+
+    private var currentTimer by mutableStateOf(InitialTimer.initialTimer)
 
     //Timer 1
-    private val timeLeft1 = mutableStateOf(START_TIME_LONG)
+    private val timeLeft1 = mutableStateOf(currentTimer.timeStart)
     var time1AsString = mutableStateOf(longToString(timeLeft1.value))
         private set
 
@@ -25,10 +36,8 @@ class TimerViewModel @Inject constructor() : ViewModel() {
         timeLeft1.value, COUNTDOWN_INTERVAL
     ) {
         override fun onTick(p0: Long) {
-            Log.d("TAG", "Millis left 1 on Tick $p0")
             time1AsString.value = longToString(p0)
             timeLeft1.value = p0
-            Log.d("LOG", "Time left 1: ${timeLeft1.value}")
         }
 
         override fun onFinish() {
@@ -37,7 +46,7 @@ class TimerViewModel @Inject constructor() : ViewModel() {
     }
 
     //Timer 2
-    private val timeLeft2 = mutableStateOf(START_TIME_LONG)
+    private val timeLeft2 = mutableStateOf(currentTimer.timeStart)
     var time2AsString = mutableStateOf(longToString(timeLeft2.value))
         private set
 
@@ -57,8 +66,28 @@ class TimerViewModel @Inject constructor() : ViewModel() {
         }
     }
 
+    init {
+        initializeTimer()
+    }
+
+    private fun initializeTimer() {
+        viewModelScope.launch {
+            try {
+                dataStore.selectedTime.collect {
+                    repository.getTimeById(it).collect { result ->
+                        currentTimer = result
+                        timeLeft1.value = currentTimer.timeStart
+                        time1AsString.value = longToString(timeLeft1.value)
+                        timeLeft2.value = currentTimer.timeStart
+                        time2AsString.value = longToString(timeLeft2.value)
+                    }
+                }
+            } catch (e: Exception) {
+            }
+        }
+    }
+
     fun startTimer() {
-        Log.d("TAG", "Time left onStart 1: ${timeLeft1.value} 2: ${timeLeft2.value}")
         when (currentTurn) {
             CurrentTurn.ONE -> {
                 timer1.cancel()
@@ -67,7 +96,6 @@ class TimerViewModel @Inject constructor() : ViewModel() {
                         Log.d("TAG", "Millis left 2 on Tick $p0")
                         time2AsString.value = longToString(p0)
                         timeLeft2.value = p0
-                        Log.d("LOG", "Time left 2: ${timeLeft2.value}")
                     }
 
                     override fun onFinish() {
@@ -85,10 +113,8 @@ class TimerViewModel @Inject constructor() : ViewModel() {
                     timeLeft1.value, COUNTDOWN_INTERVAL
                 ) {
                     override fun onTick(p0: Long) {
-                        Log.d("TAG", "Millis left 1 on Tick $p0")
                         time1AsString.value = longToString(p0)
                         timeLeft1.value = p0
-                        Log.d("LOG", "Time left 1: ${timeLeft1.value}")
                     }
 
                     override fun onFinish() {
@@ -123,15 +149,14 @@ class TimerViewModel @Inject constructor() : ViewModel() {
         Log.d("TAG", "Reset Timer Triggered")
         currentTurn = CurrentTurn.NOBODY
         timer1.cancel()
-        time1AsString.value = longToString(START_TIME_LONG)
-        timeLeft1.value = START_TIME_LONG
+        timeLeft1.value = currentTimer.timeStart
+        time1AsString.value = longToString(timeLeft1.value)
         timer2.cancel()
-        time2AsString.value = longToString(START_TIME_LONG)
-        timeLeft2.value = START_TIME_LONG
+        timeLeft2.value = currentTimer.timeStart
+        time2AsString.value = longToString(timeLeft2.value)
     }
 
     private fun longToString(time: Long): String {
-
         val seconds = (time / 1000) % 60
         val minutes = (time / (1000 * 60) % 60)
         val hours = (time / (1000 * 60 * 60) % 24)
---
+++ app/src/main/java/com/hynekbraun/chesstimer/presentation/timer/util/InitialTimer.kt
@@ -0,0 +1,9 @@
+package com.hynekbraun.chesstimer.presentation.timer.util
+
+import com.hynekbraun.chesstimer.domain.TimeModel
+
+object InitialTimer{
+    val initialTimer = TimeModel(
+        0,"", 300000L, 5000L
+    )
+}
