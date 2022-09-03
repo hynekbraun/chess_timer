30ee2dd35a7772286337968b91f52e377fcedfaa
resonus <braun.hynek@gmail.com>
2022-09-03 23:03:54 +0200

    Created AppBar for settings screen, which fixed problem with current Timer not showing in Timer Screen.

--- app/src/main/java/com/hynekbraun/chesstimer/presentation/setings/SettingsScreen.kt
+++ app/src/main/java/com/hynekbraun/chesstimer/presentation/setings/SettingsScreen.kt
@@ -1,44 +1,48 @@
 package com.hynekbraun.chesstimer.presentation.setings
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hynekbraun.chesstimer.R
-import com.hynekbraun.chesstimer.domain.TimeModel
+import com.hynekbraun.chesstimer.presentation.composables.CustomTopAppBar
 import com.hynekbraun.chesstimer.presentation.setings.composables.EmptyScreen
 import com.hynekbraun.chesstimer.presentation.setings.composables.TimeItem
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hynekbraun.chesstimer.presentation.setings.util.SettingsEvent
 
 @Composable
 fun SettingsScreen(
     modifier: Modifier = Modifier,
     viewModel: SettingsViewModel = viewModel(),
-    addTimerClicked: () -> Unit
+    addTimerClicked: () -> Unit,
+    onNavigateBack: () -> Unit
 ) {
     val scaffoldState = rememberScaffoldState()
     val state = viewModel.viewState
 
     Scaffold(
         modifier = modifier, scaffoldState = scaffoldState,
-        floatingActionButton = {
-            FloatingActionButton(onClick = { addTimerClicked() }) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = stringResource(R.string.content_desc_add_time)
-                )
-            }
-        },
-        floatingActionButtonPosition = FabPosition.End
+        topBar = {
+            CustomTopAppBar(
+                modifier = Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(bounded = false)
+                ) {
+                    addTimerClicked()
+                },
+                onNavigateBack = { onNavigateBack() }, showAddTimer = true
+            )
+        }
     ) {
         if (state.list.isEmpty()) {
             EmptyScreen()
